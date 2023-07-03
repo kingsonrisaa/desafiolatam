87a054ac7f6a15ef9db0b9c78fc9e45838e886cb
kingsonrisaa <awsingjose@gmail.com>
2023-07-02 22:45:03 -0400

    first commit

---
+++ src/views/Favoritos.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { Context } from "../Context";
+
+export default function Favoritos() {
+const {fotos, setFotos} = useContext(Context);
+
+
+
+return (
+    <div>
+      <h1>Fotos favoritas</h1>
+      <div className="p-3 galeria grid-columns-4">
+       {
+        fotos
+        .filter((foto) => foto.favorito)
+        .map((foto) => {
+         return <img src={foto.src} alt={foto.desc} key={foto.id} />;
+        })}
+      </div>
+    </div>
+  );
+}
